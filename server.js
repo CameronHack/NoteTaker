25add3c7c1fa92f9a98bfd5021e699649aca564f
Cam <camhack67@gmail.com>
2023-08-21 18:58:16 -0400

    linked notes.html in server

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const path = require('path');
 
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`Example app listening at http://127.0.0.1:${PORT}`)
 );
