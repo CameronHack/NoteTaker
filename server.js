f5344059e495c00ab3965c758a971cab7a4a3631
Cam <camhack67@gmail.com>
2023-08-21 18:57:41 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 18000;
+const path = require('path');
+
+// Serve images, css files, js files from the public directory
+// Allows us to reference files with their relative path
+// Example: http://localhost:3000/images/cat.jpg
+
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://127.0.0.1:${PORT}`)
+);
