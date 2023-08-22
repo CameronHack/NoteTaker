31ce731a8397400094a946e46102240b10777bf2
Cam <camhack67@gmail.com>
2023-08-22 19:31:27 -0400

    post notes to db.json

--- server.js
+++ server.js
@@ -2,17 +2,39 @@ const express = require('express');
 const app = express();
 const PORT = 18000;
 const path = require('path');
-
-// Serve images, css files, js files from the public directory
-// Allows us to reference files with their relative path
-// Example: http://localhost:3000/images/cat.jpg
+const fs = require('fs');
+const notes = require('./db/db.json')
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
+app.post('/api/notes', (req, res) => {
+  console.log(`notes ${req.method} request`)
+
+  const {title, text} = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    notes.push(newNote)
+
+    const notesString = JSON.stringify(notes, null, 2);
+
+    fs.writeFile(`./db/db.json`, notesString, (err) =>
+      console.log(err)
+    )
+  }
+  
+});
+
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://127.0.0.1:${PORT}`)
+  console.log(`Live at http://127.0.0.1:${PORT}`)
 );
