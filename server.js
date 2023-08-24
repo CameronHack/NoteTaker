5ce489fabda435e227b84a18df2639a79ebd39fc
Cam <camhack67@gmail.com>
2023-08-24 15:36:23 -0400

    fixed heroku port issues

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
-const PORT = process.env.port || 18000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // middleware
